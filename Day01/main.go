c13fa9711454ef64dd6c18197146902860e3f594
Leandro Deveikis <leandro.deveikis@gmail.com>
2023-12-01 12:31:15 -0300

    Day 1 complete

---
+++ Day01/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var numbersMap = map[string]string{
+	// found the hard way that when a number is replaced,
+	// it should not replace the hole number, but the letters can still be used by another one
+	"one":   "o1e",
+	"two":   "t2o",
+	"three": "t3e",
+	"four":  "f4r",
+	"five":  "f5e",
+	"six":   "s6x",
+	"seven": "s7n",
+	"eight": "e8t",
+	"nine":  "n9e",
+}
+
+func main() {
+	result1 := Challenge1()
+	fmt.Printf("Challenge 1 result: %d \n", result1)
+
+	result2 := Challenge2()
+	fmt.Printf("Challenge 2 result: %d \n", result2)
+
+}
+
+func Challenge1() int32 {
+	lines := ReadInput("Day01/input_complete")
+	return sumLines(lines)
+}
+
+func Challenge2() int32 {
+	lines := ReadInput("Day01/input_complete")
+	newLines := make([]string, 0)
+
+	for _, l := range lines {
+		for {
+			kFound := ""
+			minPos := -1
+			for k, _ := range numbersMap {
+				pos := strings.Index(l, k)
+				if pos != -1 && (pos < minPos || minPos == -1) {
+					minPos = pos
+					kFound = k
+				}
+			}
+
+			if kFound == "" {
+				break
+			}
+			l = strings.Replace(l, kFound, numbersMap[kFound], 1)
+		}
+		newLines = append(newLines, l)
+	}
+	return sumLines(newLines)
+}
+
+func sumLines(lines []string) int32 {
+	var sum int32 = 0
+	for _, l := range lines {
+		var first, second int32 = 0, 0
+		firstFound := false
+
+		for _, c := range l {
+			// 48 is 1 and 57 is 9
+			if c >= 48 && c <= 57 {
+				second = runeToNumber(c)
+				if !firstFound {
+					first = runeToNumber(c)
+					firstFound = true
+				}
+			}
+		}
+		sum += (first * 10) + second
+	}
+	return sum
+}
+
+func runeToNumber(c int32) int32 {
+	// simplest way to convert this to the number
+	return c - 48
+}
+
+func ReadInput(filename string) []string {
+	lines := make([]string, 0)
+	dat, err := os.Open(filename)
+	Check(err)
+	defer dat.Close()
+	scanner := bufio.NewScanner(dat)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
+
+func Check(err error) {
+	if err != nil {
+		panic("ERROR: " + err.Error())
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# advent-of-code-2023
+# Advent of Code - 2023
+
+Solutions for Advent of Code event for 2022 in Golang
+\- By Leandro Deveikis
+
+Day01 ⭐⭐
